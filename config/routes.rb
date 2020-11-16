753f0771b467004d63747dd1e27ad4a5ae775f75
Sy Rashid <sy.rashid.11@gmail.com>
2020-11-16 04:41:27 +0100

    FIX: Fixed some minor styling

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   # Generic Syntax
   # verb 'path/url', to: 'controller#action'
   root 'pages#home'
